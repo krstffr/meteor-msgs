e68620e1e9a45a15490bb201836b4ed3f57e06ef
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-06 12:19:11 +0200

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Package for displaying messages to the user.",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/krstffr/meteor-msgs",
   name: "krstffr:msgs"
 });
