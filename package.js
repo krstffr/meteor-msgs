4742a65a6fbce7121bcd881c130a3bcf598ef44a
Kristoffer Klintberg <krstffr@gmail.com>
2015-09-22 13:31:01 +0200

    Updated package to new version of meteor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Package for displaying messages to the user.",
-  version: "0.0.6",
+  version: "0.0.7",
   git: "https://github.com/krstffr/meteor-msgs",
   name: "krstffr:msgs"
 });
@@ -9,20 +9,17 @@ Package.onUse(function (api) {
 
 	api.versionsFrom("METEOR@0.9.0");
 
-  api.use('templating', 'client');
+  api.use(["templating", "check@1.0.5", "session"], "client");
 
-  api.add_files(['views/msgs__template.html', 'views/msgs__template.js'], 'client');
-  api.add_files('msgs.js', 'client');
+  api.add_files(["views/msgs__template.html", "views/msgs__template.js"], "client");
+  api.add_files("msgs.js", "client");
 
   // The main object.
-  api.export('Msgs', 'client');
+  api.export("Msgs", "client");
 
 });
 
 Package.on_test(function (api) {
-  api.use('krstffr:msgs');
-  api.use('tinytest');
-  api.use('test-helpers');
-  api.add_files('msgs.js', 'client');
-  api.add_files('tests/msgs-tests.js', 'client');
+  api.use(["check", "krstffr:msgs", "session", "tinytest", "underscore"]);
+  api.add_files(["msgs.js", "tests/msgs-tests.js"], "client");
 });
